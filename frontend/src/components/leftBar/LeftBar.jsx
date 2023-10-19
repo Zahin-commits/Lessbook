6a0f8a598def2d3556d9fa6346d0c79d750e5b6f
Zahin-commits <zahinhosen@gmail.com>
2023-10-20 00:08:36 +0600

    new bug fix

--- frontend/src/components/leftBar/LeftBar.jsx
+++ frontend/src/components/leftBar/LeftBar.jsx
@@ -1,4 +1,4 @@
-import './LeftBar.css';
+import './leftBar.css';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
